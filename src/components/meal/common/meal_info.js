61862a37529187aea1e5127f559cf65bf083602e
kenshin985 <kenshin985@gmail.com>
2018-10-19 15:52:02 +0200

    responsive product edit, real time calculate info about meal

--- src/components/meal/common/meal_info.js
+++ src/components/meal/common/meal_info.js
@@ -8,13 +8,13 @@ class MealInfo extends Component {
         return (
             <div>
                 <h2>Informacje o posiłku</h2>
-                <h4>Białko: {meal.protein}</h4>
-                <h4>Węglowodany: {meal.carbohydrate}</h4>
-                <h4>Tłuszcz: {meal.fat}</h4>
-                <h4>Błonnik: {meal.fibre}</h4>
-                <h4>Kcal: {meal.kcal}</h4>
-                <h4>WW: {meal.carbohydrateExchange}</h4>
-                <h4>WBT: {meal.proteinAndFatEquivalent}</h4>
+                <h4>Białko: {Math.round(meal.protein)}</h4>
+                <h4>Węglowodany: {Math.round(meal.carbohydrate)}</h4>
+                <h4>Tłuszcz: {Math.round(meal.fat)}</h4>
+                <h4>Błonnik: {Math.round(meal.fibre)}</h4>
+                <h4>Kcal: {Math.round(meal.kcal)}</h4>
+                <h4>WW: {meal.carbohydrateExchange.toFixed(2)}</h4>
+                <h4>WBT: {meal.proteinAndFatEquivalent.toFixed(2)}</h4>
             </div>
         )
     }
--- src/components/meal/meal_edit.js
+++ src/components/meal/meal_edit.js
@@ -12,7 +12,6 @@ import {
     editMeal,
     setMenuItem
 } from "../../actions";
-import MealInfo from './common/meal_info';
 import './css/meal_edit.css';
 import AuthService from "../../helpers/auth_service";
 import {LOADING_SPIN} from "../../helpers/messages";
@@ -33,7 +32,13 @@ class MealEdit extends Component {
         recipe: '',
         products: [],
         productToAdd: {},
-        amount: 0
+        protein: 0,
+        carbohydrate: 0,
+        fat: 0,
+        fibre: 0,
+        kcal: 0,
+        carbohydrateExchange: 0,
+        proteinAndFatEquivalent: 0
     };
 
     componentDidMount() {
@@ -45,7 +50,14 @@ class MealEdit extends Component {
                 name: this.props.meal.name,
                 description: this.props.meal.description,
                 recipe: this.props.meal.recipe,
-                products: this.props.meal.products
+                products: this.props.meal.products,
+                protein: this.props.meal.protein,
+                carbohydrate: this.props.meal.carbohydrate,
+                fat: this.props.meal.fat,
+                fibre: this.props.meal.fibre,
+                kcal: this.props.meal.kcal,
+                carbohydrateExchange: this.props.meal.carbohydrateExchange,
+                proteinAndFatEquivalent: this.props.meal.proteinAndFatEquivalent
             }));
         this.props.fetchProducts(0, this.state.pageSize);
     }
@@ -66,7 +78,7 @@ class MealEdit extends Component {
     };
 
     clickOnProduct = (product) => {
-        if (!_.some(this.state.products, product))
+        if (!_.some(this.state.products, (e) => e.id === product.id))
             this.setState({modalVisible: true, productToAdd: product});
         else
             message.warning("Posiłek nie może zawierać drugiego identycznego produktu")
@@ -89,7 +101,16 @@ class MealEdit extends Component {
     };
 
     onCloseTag = (product) => {
-        this.setState({products: _.filter(this.state.products, (p) => p.id !== product.id)});
+        this.setState({
+            products: _.filter(this.state.products, (p) => p.id !== product.id),
+            protein: this.state.protein - product.protein * product.amount / 100,
+            carbohydrate: this.state.carbohydrate - product.carbohydrate * product.amount / 100,
+            fat: this.state.fat - product.fat * product.amount / 100,
+            fibre: this.state.fibre - product.fibre * product.amount / 100,
+            kcal: this.state.kcal - product.kcal * product.amount / 100,
+            carbohydrateExchange: this.state.carbohydrateExchange - product.carbohydrateExchange * product.amount / 100,
+            proteinAndFatEquivalent: this.state.proteinAndFatEquivalent - product.proteinAndFatEquivalent * product.amount / 100,
+        });
         message.success("Usunięto " + product.name);
     };
 
@@ -133,24 +154,87 @@ class MealEdit extends Component {
     };
 
     render() {
+        if (!this.props.meal)
+            return LOADING_SPIN;
+
         const {meal} = this.props;
         const {Search} = Input;
-        const {searchValue, pageSize} = this.state;
+        const {searchValue, pageSize, name} = this.state;
         const {content} = this.props.products;
-
-        if (!meal)
-            return LOADING_SPIN;
 
         return (
             <div className='content'>
-                <div className='content__mealEdit'>
-                    <div className='head'>
-                        <Input value={this.state.name ? this.state.name : meal.name}
-                               onChange={(e) => this.setState({name: e.target.value})}/>
+                <div className='editMeal__content'>
+                    <div className='editMeal__head'>
+                        <Input value={name ? name : meal.name}
+                               onChange={(e) => this.setState({name: e.target.value})}
+                               className='editMeal__head--name'/>
                     </div>
-                    <div className='body'>
-                        <div className='edit__leftPanel'>
-                            <div className='edit__mealProducts'>
+                    <div className='editMeal__body'>
+                        <div className='editMeal__displayMeal'>
+                            <div className='editMeal__displayMeal--first'>
+                                <div className='editMeal__imageContainer'>
+                                    <img src={meal.imageUrl} alt={meal.name} className='editMeal__imageContainer--image'/>
+                                </div>
+                                <div className='editMeal__info--core'>
+                                    <h2>Informacje o posiłku</h2>
+                                    <h4>Białko: {Math.round(this.state.protein)}</h4>
+                                    <h4>Węglowodany: {Math.round(this.state.carbohydrate)}</h4>
+                                    <h4>Tłuszcz: {Math.round(this.state.fat)}</h4>
+                                    <h4>Błonnik: {Math.round(this.state.fibre)}</h4>
+                                    <h4>Kcal: {Math.round(this.state.kcal)}</h4>
+                                    <h4>WW: {this.state.carbohydrateExchange.toFixed(2)}</h4>
+                                    <h4>WBT: {this.state.proteinAndFatEquivalent.toFixed(2)}</h4>
+                                </div>
+                            </div>
+                            <div className='editMeal__displayMeal--second'>
+                                <div className='editMeal__description'>
+                                    <h2>Opis</h2>
+                                    <TextArea
+                                        value={this.state.description ? this.state.description : meal.description}
+                                        onChange={(e) => this.setState({description: e.target.value})}
+                                        rows={6}/>
+                                </div>
+                                <div className='editMeal__recipe'>
+                                    <h2>Przepis</h2>
+                                    <TextArea value={this.state.recipe ? this.state.recipe : meal.recipe}
+                                              onChange={(e) => this.setState({recipe: e.target.value})}
+                                              rows={6}/>
+                                </div>
+                            </div>
+                        </div>
+                        <Modal
+                            title="Wpisz ilość produktu [g]"
+                            visible={this.state.modalVisible}
+                            onOk={() => {
+                                const {productToAdd} = this.state;
+                                this.setState({products: [...this.state.products, productToAdd]});
+                                this.setState({
+                                    productToAdd: {},
+                                    modalVisible: false,
+                                    protein: this.state.protein + productToAdd.protein * productToAdd.amount / 100,
+                                    carbohydrate: this.state.carbohydrate + productToAdd.carbohydrate * productToAdd.amount / 100,
+                                    fat: this.state.fat + productToAdd.fat * productToAdd.amount / 100,
+                                    fibre: this.state.fibre + productToAdd.fibre * productToAdd.amount / 100,
+                                    kcal: this.state.kcal + productToAdd.kcal * productToAdd.amount / 100,
+                                    carbohydrateExchange: this.state.carbohydrateExchange + productToAdd.carbohydrateExchange * productToAdd.amount / 100,
+                                    proteinAndFatEquivalent: this.state.proteinAndFatEquivalent + productToAdd.proteinAndFatEquivalent * productToAdd.amount / 100,
+                                });
+                                message.success("Dodano produkt");
+
+                            }}
+                            onCancel={() => this.setState({modalVisible: false})}
+                        >
+                            <InputNumber min={0} max={900} value={this.state.productToAdd.amount} onChange={(value) => {
+                                this.setState({
+                                    productToAdd: {...this.state.productToAdd, amount: value}
+                                })
+                                ;
+                            }}/>
+
+                        </Modal>
+                        <div className='editMeal_searchProducts'>
+                            <div className='editMeal__products'>
                                 <h2>Produkty</h2>
                                 {_.map(this.state.products, product =>
                                     <Tag key={product.id} style={{marginBottom: '5px'}} closable
@@ -159,52 +243,6 @@ class MealEdit extends Component {
                                     </Tag>
                                 )}
                             </div>
-                            <div className='leftPanel__bottom'>
-                                <div className='leftPanel__bottom--left'>
-                                    <div className='edit__imageContainer'>
-                                        <img src={meal.imageUrl} alt={meal.name} className='edit__imageContainer--image'/>
-                                    </div>
-                                    <div className='edit__mealInfo'>
-                                        <MealInfo meal={meal}/>
-                                    </div>
-                                </div>
-                                <div className='leftPanel__bottom--right'>
-                                    <div className='edit__mealDescription'>
-                                        <h2>Opis</h2>
-                                        <TextArea
-                                            value={this.state.description ? this.state.description : meal.description}
-                                            onChange={(e) => this.setState({description: e.target.value})}
-                                            rows={6}/>
-                                    </div>
-                                    <div className='edit__mealRecipe'>
-                                        <h2>Przepis</h2>
-                                        <TextArea value={this.state.recipe ? this.state.recipe : meal.recipe}
-                                                  onChange={(e) => this.setState({recipe: e.target.value})}
-                                                  rows={6}/>
-                                    </div>
-                                </div>
-                            </div>
-                            <Button onClick={this.clickOnSaveButton}>Zapisz zmiany</Button>
-                        </div>
-                        <Modal
-                            title="Basic Modal"
-                            visible={this.state.modalVisible}
-                            onOk={() => {
-                                const {productToAdd} = this.state;
-                                productToAdd.amount = this.state.amount;
-                                this.setState({products: [...this.state.products, productToAdd]});
-                                this.setState({productToAdd: {}, modalVisible: false, amount: 0});
-                                message.success("Dodano produkt");
-
-                            }}
-                            onCancel={() => this.setState({modalVisible: false})}
-                        >
-                            <InputNumber min={0} max={900} value={this.state.amount} onChange={(value) => {
-                                this.setState({amount: value});
-                            }}/>
-
-                        </Modal>
-                        <div className='edit__rightPanel'>
                             <Search
                                 placeholder="Szukaj produktów"
                                 onSearch={searchValue => {
@@ -220,9 +258,12 @@ class MealEdit extends Component {
                                 className='search'
                             />
                             {!content ? <div className='content loading-spin'><Spin size='large'/></div> : this.renderCollapse()}
+
                         </div>
                     </div>
+
                 </div>
+                <Button onClick={this.clickOnSaveButton} type='primary' size='large' className='saveButton'>Zapisz zmiany</Button>
             </div>
         );
     }
